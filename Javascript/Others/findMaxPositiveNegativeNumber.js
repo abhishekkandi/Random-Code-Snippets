b3b3714f65896a1a43abcf288cdf8663c189c694
abhishekkandi <kandii.abhishek@gmail.com>
2020-02-22 20:28:55 +0530

    findMaxPositiveNegativeNumber.js

---
+++ Javascript/Others/findMaxPositiveNegativeNumber.js
@@ -0,0 +1,21 @@
+function solution(A) {
+    let maximumValue = findMaxFromGivenArray(A);
+    
+    if(maximumValue > 0){
+        while(A.length != 0){
+            if(A.indexOf(-maximumValue) !== -1){
+                return maximumValue
+            } else {
+                A = A.filter(x => x != maximumValue);
+                maximumValue = findMaxFromGivenArray(A)
+            }   
+        }
+    }
+    return 0;
+}
+
+function findMaxFromGivenArray(A){
+    return Math.max(...A);
+}
+
+console.log(solution([1,-1,10,9,2]))//returns 1
